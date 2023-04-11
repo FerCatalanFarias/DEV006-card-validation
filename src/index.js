9f81aceef1f35afb3f5226c5f3c84da21ba32e28
Fer <fer.design.cf@gmail.com>
2023-04-11 15:33:59 -0400

    código nuevo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+const donarboton = document.getElementById("donar");
+
+//const verificaBtn = document.getElementById("verificacion");
+
+const numeroTarjeta = document.getElementById("numerotarjeta");
+
+//const fechaVenc = document.getElementById("fechaVenc");
+
+
+
+
+
+
+
+
+
+donarboton.addEventListener("click", function () {
+  const cardnumber = numeroTarjeta.value;
+  const val = validator.isValid(cardnumber);
+  const mask = validator.maskify(cardnumber);
+
+
+
+  if (val === true) {
+    alert("Su tarjeta n° " + mask + " es válida :) ¡GRACIAS POR SU APORTE ES MUY VALIOSO!");
+  }
+
+  else {
+    alert("Su tarjeta n° " + mask + " no es válida :( ¡Inténtelo nuevamente!");
+  }
+});
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,105 @@
 const validator = {
-  // ...
+
+  isValid: function (numeroTarjeta) {
+
+
+    //console.log(numeroTarjeta);
+
+
+    const tarjetaArray = numeroTarjeta.split("");
+
+
+    tarjetaArray.reverse();
+
+
+    //console.log(tarjetaArray);
+
+
+    let sumaTotal = 0;
+
+
+    for (let index = 0; index < tarjetaArray.length; index++) {
+
+
+      if (index % 2 !== 0) {
+
+
+        //console.log("index" + index + " es impar");
+
+
+        //console.log("valor inicial" + tarjetaArray [index]);
+
+
+        tarjetaArray[index] = tarjetaArray[index] * 2;
+
+
+        //console.log("valor duplicado" + tarjetaArray[index]);
+
+
+        if (tarjetaArray[index] >= 10) {
+
+
+          const mayorAdiez = String(tarjetaArray[index]).split("");
+
+
+          tarjetaArray[index] =
+            parseInt(mayorAdiez[0]) + parseInt(mayorAdiez[1]);
+
+
+          //console.log("suma de los digitos " + tarjetaArray[index]);
+        }
+      }
+
+      sumaTotal = sumaTotal + parseInt(tarjetaArray[index]);
+    }
+
+    //console.log(sumaTotal);
+
+
+    let resultado;
+
+
+    if (sumaTotal % 10 === 0) {
+
+      //console.log("Tarjeta válida");
+      resultado = true;
+    } else {
+
+      //console.log("Tarjeta inválida");
+      resultado = false;
+    }
+
+    return resultado;
+  },
+
+
+
+
+  //definir metodo maskify
+  maskify: function (numeroTarjeta) {
+
+    // se define la variable tarjetaArray como un array
+    const tarjetaArray = numeroTarjeta.split("");
+
+    // se condiciona, si el length del array es mayor de 4 dígitos, se sustituyen los números por # exceptuando los últimos 4 números
+    if (tarjetaArray.length > 4) {
+      for (let index = 0; index < tarjetaArray.length - 4; index++) {
+        tarjetaArray[index] = "#"
+        //console.log(tarjetaArray);
+      }
+
+      // si es el lenght es menor a 4 no se sustituyen
+    } else {
+      //console.log(tarjetaArray);
+    }
+    // se presentan los numeros de array a string, definiendo una variable numerooculto y utilizando el .Join 
+    const numerooculto = tarjetaArray.join("");
+    return numerooculto;
+  }
+
+
 };
 
+
 export default validator;
+
